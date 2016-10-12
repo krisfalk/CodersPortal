4ff74e814fde47d149616bc839d192535b893415
krisfalk <krisfalk2007@hotmail.com>
2016-10-12 15:27:45 -0500

    initial upload

---
+++ CodersPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodersPortal.Startup))]
+namespace CodersPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
